aa2ffc450be6816e6ab7e0fc34c92a103f9f74e1
Felix Yeung <19355619+imfelixyeung@users.noreply.github.com>
2026-04-30 23:18:19 +0100

    feat: implement writer

--- classes/writer.php
+++ classes/writer.php
@@ -17,6 +17,10 @@
 namespace dataformat_sqlite;
 
 use core\dataformat\base;
+use core_text;
+use PDO;
+use PDOStatement;
+use function count;
 
 /**
  * SQLite Database format writer.
@@ -27,14 +31,112 @@ use core\dataformat\base;
  */
 class writer extends base {
     /** @var string */
-    public $mimetype = 'application/vnd.sqlite3';
+    protected $mimetype = 'application/vnd.sqlite3';
 
     /** @var string $extension */
-    public $extension = '.sqlite';
+    protected $extension = '.sqlite';
+
+    /** @var string[] $columns */
+    protected $columns = [];
+
+    /** @var string */
+    protected string $sqlitepath;
+
+    /** @var string */
+    protected string $tablename = 'export';
+
+    /** @var PDO */
+    protected PDO $pdo;
+
+    /** @var PDOStatement */
+    protected PDOStatement $insertstatement;
 
     /**
-     * {@inheritDoc}
+     * Initialise the database that we will be adding data to.
+     *
+     * @param string[] $columns
+     * @return void
+     */
+    public function start_sheet($columns) {
+        $this->columns = array_map(
+            fn($column) => core_text::strtolower(clean_param($column, PARAM_ALPHANUMEXT)),
+            $columns,
+        );
+
+        $this->sqlitepath = make_request_directory() . '/export.sqlite';
+        $this->create_database();
+        $this->create_table();
+        $this->prepare_insert_statement();
+        return;
+    }
+
+    /**
+     * Creates the PDO for SQLite database.
+     * @return void
+     */
+    protected function create_database(): void {
+        $this->pdo = new PDO("sqlite:$this->sqlitepath");
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    /**
+     * Creates the table for export.
+     * @return void
+     */
+    protected function create_table(): void {
+        $sqlcreatecolumns = [
+            'row INTEGER PRIMARY KEY',
+            ...array_map(fn($column) => "`col_$column` TEXT", $this->columns),
+        ];
+        $sqlcreatecolumnsstr = implode(', ', $sqlcreatecolumns);
+        $sql = "CREATE TABLE IF NOT EXISTS {$this->tablename} ($sqlcreatecolumnsstr)";
+        $this->pdo->exec($sql);
+        return;
+    }
+
+    /**
+     * Creates the prepared statement for inserting data into the database.
+     * @return void
+     */
+    protected function prepare_insert_statement(): void {
+        $sqlinsertcolumns = implode(',', array_fill(0, count($this->columns) + 1, '?'));
+        $this->insertstatement = $this->pdo->prepare("INSERT INTO {$this->tablename} VALUES ($sqlinsertcolumns)");
+        return;
+    }
+
+    /**
+     * Write a single record to DB.
+     *
+     * @param string[] $record
+     * @param int $rownum
+     * @return void
      */
     public function write_record($record, $rownum) {
+        $this->insertstatement->execute([$rownum, ...$record]);
+        return;
+    }
+
+    /**
+     * Cleanup.
+     *
+     * @param string[] $columns
+     * @return void
+     */
+    public function close_sheet($columns) {
+        $this->pdo = null;
+        $this->insertstatement = null;
+    }
+
+    /**
+     * Send the entire sqlite dump.
+     * @return void
+     */
+    public function close_output() {
+        if (ob_get_level() > 0) {
+            echo file_get_contents($this->sqlitepath);
+        } else {
+            readfile($this->sqlitepath);
+        }
+        return;
     }
 }
